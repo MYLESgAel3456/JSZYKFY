9e1f4e8e8a68a6633a14a94dc58b63d66d3d35f3
MYLESgAel3456 <galley.humble.0p@icloud.com>
2025-08-08 23:51:56 +0900

    Setup 配置文件管理器

---
+++ config_manager_0808_2251_dpb.js
@@ -0,0 +1,83 @@
+// 代码生成时间: 2025-08-08 22:51:55
+(function() {
+    "use strict";
+
+    // ConfigManager constructor
+    function ConfigManager(configData) {
+        this.configData = configData || {};
+    }
+
+    // Public API methods
+    ConfigManager.prototype = {
+
+        /**
+         * Load configuration data from a JSON file
+         * @param {string} filePath - The path to the JSON configuration file
+         * @returns {Promise} - A promise that resolves with the configuration data
+         */
+        loadConfig: function(filePath) {
+            return new Promise((resolve, reject) => {
+                d3.json(filePath)
+                    .then(configData => {
+                        this.configData = configData;
+                        resolve(configData);
+                    })
+                    .catch(error => {
+                        console.error('Error loading configuration:', error);
+                        reject(error);
+                    });
+            });
+        },
+
+        /**
+         * Update a configuration setting
+         * @param {string} key - The key of the configuration setting to update
+         * @param {any} value - The new value for the configuration setting
+         * @returns {void}
+         */
+        updateConfig: function(key, value) {
+            if (typeof this.configData[key] !== 'undefined') {
+                this.configData[key] = value;
+            } else {
+                console.warn(`Config key '${key}' not found.`);
+            }
+        },
+
+        /**
+         * Save the current configuration to a JSON file
+         * @param {string} filePath - The path to save the JSON configuration file
+         * @returns {Promise} - A promise that resolves when the configuration is saved
+         */
+        saveConfig: function(filePath) {
+            return new Promise((resolve, reject) => {
+                d3.json(filePath, this.configData)
+                    .then(() => {
+                        resolve();
+                    })
+                    .catch(error => {
+                        console.error('Error saving configuration:', error);
+                        reject(error);
+                    });
+            });
+        },
+
+        /**
+         * Retrieve a configuration setting by key
+         * @param {string} key - The key of the configuration setting to retrieve
+         * @returns {any} - The value of the configuration setting
+         */
+        getConfig: function(key) {
+            return this.configData[key];
+        }
+    };
+
+    // Export the ConfigManager class
+    // If using ES6 modules, use `export default ConfigManager;`
+    // For CommonJS modules, use `module.exports = ConfigManager;`
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = ConfigManager;
+    } else {
+        window.ConfigManager = ConfigManager;
+    }
+
+})();
